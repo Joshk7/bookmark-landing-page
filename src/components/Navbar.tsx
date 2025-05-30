83f6619edfccd752c7a205cf8f63bc2fb04e7989
JoshK7 <joshk7683@gmail.com>
2025-05-29 21:15:30 -0500

    added changes to navbar on desktop

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,9 +7,43 @@ const Navbar = () => {
       <a href="#Home">
         <img src={bookmark} height={25} alt="" />
       </a>
-      <button>
+      <button className="md:hidden">
         <img src={hamburger} height={15} alt="" />
       </button>
+      <ul className="hidden md:flex gap-x-12">
+        <li>
+          <a
+            className="text-[0.875rem] text-[var(--blue-950)] hover:text-[var(--red)] leading-loose uppercase"
+            href="#Features"
+          >
+            Features
+          </a>
+        </li>
+        <li>
+          <a
+            className="text-[0.875rem] text-[var(--blue-950)] hover:text-[var(--red)] leading-loose uppercase"
+            href="#Pricing"
+          >
+            Pricing
+          </a>
+        </li>
+        <li>
+          <a
+            className="text-[0.875rem] text-[var(--blue-950)] hover:text-[var(--red)] leading-loose uppercase"
+            href="#Contact"
+          >
+            Contact
+          </a>
+        </li>
+        <li>
+          <a
+            className="px-8 py-2 text-[0.875rem] text-[var(--white)] hover:text-[var(--red)] bg-[var(--red)] hover:bg-[var(--white)] hover:outline-2 leading-loose uppercase rounded-[0.3125rem]"
+            href="#Login"
+          >
+            Login
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 };
