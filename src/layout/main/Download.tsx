65a8f596d37d4d8ffb28cec191debda27c85ce77
JoshK7 <joshk7683@gmail.com>
2025-06-15 20:41:45 -0700

    text styling for download cards

--- src/layout/main/Download.tsx
+++ src/layout/main/Download.tsx
@@ -4,33 +4,65 @@ const Download = () => {
   return (
     <section className="px-8 py-[9.875rem]">
       <div className="max-w-[540px] mx-auto">
-        <h2 className="text-center font-medium text-2xl sm:text-[2rem] leading-[3.25rem] tracking-[-0.005rem] sm:tracking-[-0.00625rem]">Download the extension</h2>
+        <h2 className="text-center font-medium text-2xl sm:text-[2rem] leading-[3.25rem] tracking-[-0.005rem] sm:tracking-[-0.00625rem]">
+          Download the extension
+        </h2>
         <p className="text-center text-[var(--blue-950)] text-[0.9375rem] sm:text-[1.125rem] leading-[165%] sm:leading-[1.75rem] opacity-50">
           We’ve got more browsers in the pipeline. Please do let us know if
           you’ve got a favourite you’d like us to prioritize.
         </p>
       </div>
-      <ul className="mx-auto flex flex-wrap pt-10 px-4 gap-y-10">
+      <ul className="mx-auto flex flex-wrap pt-10 px-4 gap-y-10 gap-x-2">
         <li className="mx-auto flex flex-col">
-            <img className="mx-auto mb-8" src="/src/assets/logo-chrome.svg" alt="" />
-            <h2 className="text-center mb-2">Add to Chrome</h2>
-            <p className="text-center mb-6">Minimum version 62</p>
-            <img className="mb-6" src="/src/assets/bg-dots.svg" alt="" />
-            <Button className="mx-auto" primary>Add & Install Extension</Button>
+          <img
+            className="mx-auto mb-8"
+            src="/src/assets/logo-chrome.svg"
+            alt=""
+          />
+          <h2 className="text-center mb-2 text-2xl font-medium leading-loose sm:text-[1.25rem] sm:tracking-[0.015625rem] sm:leading-[120%]">
+            Add to Chrome
+          </h2>
+          <p className="text-center mb-6 text-[0.9375rem] leading-[185%] opacity-50 sm:text-[0.875rem] sm:leading-[200%]">
+            Minimum version 62
+          </p>
+          <img className="mb-6" src="/src/assets/bg-dots.svg" alt="" />
+          <Button className="mx-auto" primary>
+            Add & Install Extension
+          </Button>
         </li>
         <li className="mx-auto flex flex-col">
-            <img className="mx-auto mb-8" src="/src/assets/logo-firefox.svg" alt="" />
-            <h2 className="text-center mb-2">Add to Firefox</h2>
-            <p className="text-center mb-6">Minimum version 55</p>
-            <img className="mb-6" src="/src/assets/bg-dots.svg" alt="" />
-            <Button className="mx-auto" primary>Add & Install Extension</Button>
+          <img
+            className="mx-auto mb-8"
+            src="/src/assets/logo-firefox.svg"
+            alt=""
+          />
+          <h2 className="text-center mb-2 text-2xl font-medium leading-loose sm:text-[1.25rem] sm:tracking-[0.015625rem] sm:leading-[120%]">
+            Add to Firefox
+          </h2>
+          <p className="text-center mb-6 text-[0.9375rem] leading-[185%] opacity-50 sm:text-[0.875rem] sm:leading-[200%]">
+            Minimum version 55
+          </p>
+          <img className="mb-6" src="/src/assets/bg-dots.svg" alt="" />
+          <Button className="mx-auto" primary>
+            Add & Install Extension
+          </Button>
         </li>
         <li className="mx-auto flex flex-col">
-            <img className="mx-auto mb-8" src="/src/assets/logo-opera.svg" alt="" />
-            <h2 className="text-center mb-2">Add to Opera</h2>
-            <p className="text-center mb-6">Minimum version 46</p>
-            <img className="mb-6" src="/src/assets/bg-dots.svg" alt="" />
-            <Button className="mx-auto" primary>Add & Install Extension</Button>
+          <img
+            className="mx-auto mb-8"
+            src="/src/assets/logo-opera.svg"
+            alt=""
+          />
+          <h2 className="text-center mb-2 text-2xl font-medium leading-loose sm:text-[1.25rem] sm:tracking-[0.015625rem] sm:leading-[120%]">
+            Add to Opera
+          </h2>
+          <p className="text-center mb-6 text-[0.9375rem] leading-[185%] opacity-50 sm:text-[0.875rem] sm:leading-[200%]">
+            Minimum version 46
+          </p>
+          <img className="mb-6" src="/src/assets/bg-dots.svg" alt="" />
+          <Button className="mx-auto" primary>
+            Add & Install Extension
+          </Button>
         </li>
       </ul>
     </section>
